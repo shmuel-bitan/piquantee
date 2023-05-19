e214edce294e45e662d1fa673cf79fd9a58cca48
shmuel-bitan <112951324+shmuel-bitan@users.noreply.github.com>
2023-05-19 12:41:28 +0200

    connexion avec le back mit en ligne sur render

--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -16,7 +16,7 @@ export class AuthService {
               private router: Router) {}
 
   createUser(email: string, password: string) {
-    return this.http.post<{ message: string }>('http://localhost:3000/api/auth/signup', {email: email, password: password});
+    return this.http.post<{ message: string }>('https://bc-22pc.onrender.com/api/auth/signup', {email: email, password: password});
   }
 
   getToken() {
@@ -28,7 +28,7 @@ export class AuthService {
   }
 
   loginUser(email: string, password: string) {
-    return this.http.post<{ userId: string, token: string }>('http://localhost:3000/api/auth/login', {email: email, password: password}).pipe(
+    return this.http.post<{ userId: string, token: string }>('https://bc-22pc.onrender.com/api/auth/login', {email: email, password: password}).pipe(
       tap(({ userId, token }) => {
         this.userId = userId;
         this.authToken = token;
